26e20f65306a0bc53c7c39fecac5551e88039c4a
Ric Richards <richardsric7@gmail.com>
2022-05-26 22:30:19 +0100

    initial commit

--- internal/components/users/models/user.go
+++ internal/components/users/models/user.go
@@ -3,28 +3,28 @@ package users
 import "time"
 
 type User struct {
-	CreatedAt             time.Time    `json:"createdAt"`
-	UpdatedAt             time.Time    `json:"updatedAt"`
-	LastUpdatedMobileOn   time.Time    `json:"lastUpdatedMobileOn"`
-	ID                    string       `json:"id"`
-	Username              string       `gorm:"size:16; index:idx_user_unique_username, unique" json:"username"`
-	Email                 string       `gorm:"size:45; index:idx_user_unique_email, unique" json:"email"`
-	ImageThumbnailURL     *string      `json:"imageThumbnailURL"`
-	FirstName             string       `gorm:"size:50" json:"firstName"`
-	LastName              string       `gorm:"size:50" json:"lastName"`
-	Mobile                *string      `gorm:"size:16" json:"mobile"`
-	PublicKey             string       `gorm:"size:56; index:idx_user_unique_public_key, unique" json:"publicKey"`
+	CreatedAt           time.Time `json:"createdAt"`
+	UpdatedAt           time.Time `json:"updatedAt"`
+	LastUpdatedMobileOn time.Time `json:"lastUpdatedMobileOn"`
+	ID                  string    `json:"id"`
+	Username            string    `gorm:"size:16; index:idx_user_unique_username, unique" json:"username"`
+	Email               string    `gorm:"size:45; index:idx_user_unique_email, unique" json:"email"`
+	ImageThumbnailURL   *string   `json:"imageThumbnailURL"`
+	FirstName           string    `gorm:"size:50" json:"firstName"`
+	LastName            string    `gorm:"size:50" json:"lastName"`
+	Mobile              *string   `gorm:"size:16" json:"mobile"`
+	PublicKey           string    `gorm:"size:56; index:idx_user_unique_public_key, unique" json:"publicKey"`
 	// TempPublicKey         *string      `gorm:"size:56;index:idx_user_temp_public_key;null"`
-	Referrer              *string      `gorm:"size:16" json:"referrer"`
+	Referrer              *string      `gorm:"size:16; index:idx_user_referrer" json:"referrer"`
 	ReferralLink          *string      `json:"referralLink"`
 	ReferralQrCode        *string      `json:"referralQrCode"`
 	PushNotificationToken *string      `json:"pushNotificationToken"`
-	Corporate             uint         `gorm:"type:tinyint; default:0" json:"corporate"`
-	MobileVerified        uint         `gorm:"type:tinyint; default:0" json:"mobileVerified"`
-	MemershipType         uint         `gorm:"type:tinyint; default:0" json:"membershipType"`
+	Corporate             uint         `gorm:"type:integer;not null; default:0" json:"corporate"`
+	MobileVerified        uint         `gorm:"type:integer;not null; default:0" json:"mobileVerified"`
+	MemershipType         uint         `gorm:"type:integer;not null; default:0" json:"membershipType"`
 	MembershipExpiry      *time.Time   `json:"membershipExpiry"`
-	KYCVerified           uint         `gorm:"type:tinyint; default:0" json:"kycVerified"`
-	WalletRecoveryEnabled uint         `gorm:"type:tinyint; default:0" json:"walletRecoveryEnabled"`
+	KYCVerified           uint         `gorm:"type:integer;not null; default:0" json:"kycVerified"`
+	WalletRecoveryEnabled uint         `gorm:"type:integer;not null; default:0" json:"walletRecoveryEnabled"`
 	UserWallets           []UserWallet `json:"userWallets"`
 	PublicIP              string       `gorm:"size:45" json:"publicIP"`
 	CountryCode           *string      `gorm:"size:2;null"`
@@ -50,9 +50,9 @@ type UserWallet struct {
 	Link                    *string                 `json:"link"`
 	QrCode                  *string                 `json:"qrCode"`
 	Alias                   string                  `gorm:"size:27; index:idx_unique_alias, unique" json:"alias"` //primaryUsername_tag for sub wallets
-	Signer                  string                  `gorm:"size:56; index:idx_sub_wallet_signer" json:"signer"`   //if ID is same as signer, then it is a primary wallet
-	UserID                  string                  `json:"userId"`
-	ManagedAccessEnabled    uint                    `gorm:"type:tinyint; default:0" json:"managedAccessEnabled"`
+	Signer                  string                  `gorm:"size:56; index:idx_user_wallet_signer" json:"signer"`  //if ID is same as signer, then it is a primary wallet
+	UserID                  string                  `gorm:"type:integer;not null; default:0;index:idx_user_wallets_user_id" json:"userId"`
+	ManagedAccessEnabled    uint                    `gorm:"type:integer;not null; default:0" json:"managedAccessEnabled"`
 	UserWalletManagedAccess UserWalletManagedAccess `json:"userWalletManagedAccess"`
 }
 
@@ -60,14 +60,14 @@ type UserWalletManagedAccess struct {
 	CreatedAt           time.Time      `json:"createdAt"`
 	UpdatedAt           time.Time      `json:"updatedAt"`
 	ID                  string         `gorm:"" json:"accessId"`
-	UserWalletID        string         `gorm:"size:56" json:"publicKey"`
-	NumberOfAuthorizers uint           `gorm:"type:tinyint; default:1" json:"numberOfAuthorizers"`
+	UserWalletID        string         `gorm:"size:56; index:idx_manage_access_user_wallet_id" json:"publicKey"`
+	NumberOfAuthorizers uint           `gorm:"type:integer; default:1" json:"numberOfAuthorizers"`
 	AccessList          []WalletAccess `json:"accessList"`
 }
 type WalletAccess struct {
 	Username                  string `gorm:"size:16; primaryKey" json:"username"`
 	AccessLevel               string `gorm:"size:10" json:"accessLevel"`
-	UserWalletManagedAccessID string `gorm:"size:56" json:"userWalletManagedAccessId"`
+	UserWalletManagedAccessID string `gorm:"index:idx_wallet_access_wallet_access_id" json:"userWalletManagedAccessId"`
 }
 
 type AccessLevel struct {
