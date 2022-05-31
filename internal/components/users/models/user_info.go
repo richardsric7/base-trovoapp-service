f6de0ec1d7c3223fee4d5f4ef95675eb031e3be6
Ric Richards <richardsric7@gmail.com>
2022-05-31 16:13:44 +0100

    initial commit

--- internal/components/users/models/user_info.go
+++ internal/components/users/models/user_info.go
@@ -2,7 +2,7 @@ package users
 
 type UserInfo struct {
 	UserData               UserJSON                 `json:"userData"`
-	AssetBalances          map[string]AssetBalances `json:"assetBalances"` //map of wallet public key and the asset balances
-	NFTBalances            map[string]NFTBalances   `json:"nftBalances"`   //map of nft wallet and nftBalances
-	ThirdPartyWalletAccess []ThirdPartyWalletAccess `json:"thirdPartyWalletAccess"`
+	AssetBalances          map[string]AssetBalances `json:"assetBalances"`          //map of wallet public key and the asset balances
+	NFTBalances            map[string]NFTBalances   `json:"nftBalances"`            //map of nft wallet and nftBalances
+	ThirdPartyWalletAccess []ThirdPartyWalletAccess `json:"thirdPartyWalletAccess"` //shows all the third party access granted to this user
 }
