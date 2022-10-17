14972f2a77844cadd9f41a553740a8d3d50bd1cf
Ric Richards <richardsric7@gmail.com>
2022-10-17 09:54:30 +0100

    update

--- internal/components/payments/models/tracked_models.go
+++ internal/components/payments/models/tracked_models.go
@@ -3,10 +3,11 @@ package payments
 import "github.com/gofrs/uuid"
 
 type TrackedWallet struct {
-	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
-	PublicKey string    `gorm:"index:idx_tracked_wallet_public_key,unique"`
-	Alias     string    `gorm:"index:idx_tracked_wallet_alias"`
-	Name      string    `gorm:"index:idx_tracked_wallet_name"`
+	ID            uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
+	PublicKey     string    `gorm:"index:idx_tracked_wallet_public_key,unique"`
+	TempPublicKey *string   `gorm:"index:idx_tracked_wallet_temp_public_key,unique"`
+	Alias         string    `gorm:"index:idx_tracked_wallet_alias"`
+	Name          string    `gorm:"index:idx_tracked_wallet_name"`
 }
 
 type TrackedPublicKey struct {
