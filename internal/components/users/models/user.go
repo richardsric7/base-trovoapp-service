22fedfcb7bad5455f287df0322d2741daff10708
Ric Richards <richardsric7@gmail.com>
2022-10-17 14:30:21 +0100

    update

--- internal/components/users/models/user.go
+++ internal/components/users/models/user.go
@@ -168,7 +168,7 @@ type SubWalletInfo struct {
 	ChannelAccount          string   `json:"channelAccount"`
 	ChannelAccountSignature string   `json:"channelAccountSignature"`
 	SubWalletMustSign       int      `json:"subWalletMustSign"`
-	AssetIsserWallet        int      `json:"asetIssuerWallet"`
+	AssetIsserWallet        int      `json:"assetIssuerWallet"`
 	Messages                []string `json:"messages"`
 }
 
