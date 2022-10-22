4881c5c1eef4d1b33c0a9b0c34948f3f5f13a53c
Ric Richards <richardsric7@gmail.com>
2022-10-22 20:49:47 +0100

    update

--- internal/components/users/controllers/main.go
+++ internal/components/users/controllers/main.go
@@ -1947,7 +1947,12 @@ func Init(router *gin.Engine, gc *sharedconfig.GlobalConfig) {
 				if u.PushNotificationToken != nil && v.Permission != "VIEW-ONLY" {
 					dataPayload := make(map[string]string)
 					dataPayload["none"] = ""
-					pns.SendFirebaseMessage(*u.PushNotificationToken, fmt.Sprintf("%v Submitted an approval on wallet %v!", signerUser.Username, wallet.Alias), fmt.Sprintf("%v submitted an approval for Request:\n%v\nApproval stage is now %v/%v", signerUser.Username, approvalRequest.Description, approvalRequest.ApprovalsGotten, approvalRequest.ApprovalsNeeded), "", dataPayload, gc.PushNotificationClient, gc.PNSContext)
+					if approvalRequest.TransactionStatus != "COMPLETED" {
+						pns.SendFirebaseMessage(*u.PushNotificationToken, fmt.Sprintf("%v Submitted an approval on wallet %v!", signerUser.Username, wallet.Alias), fmt.Sprintf("%v submitted an approval for Request:\n%v\nApproval stage is now %v/%v", signerUser.Username, approvalRequest.Description, approvalRequest.ApprovalsGotten, approvalRequest.ApprovalsNeeded), "", dataPayload, gc.PushNotificationClient, gc.PNSContext)
+
+					} else {
+						pns.SendFirebaseMessage(*u.PushNotificationToken, fmt.Sprintf("%v completed the %v approval on wallet %v!", signerUser.Username, approvalRequest.TransactionType, wallet.Alias), fmt.Sprintf("%v completed the %v request:\n%v", signerUser.Username, approvalRequest.TransactionType, approvalRequest.Description), "", dataPayload, gc.PushNotificationClient, gc.PNSContext)
+					}
 				}
 			}
 		}
