3d26151a729b2248cce14748dfd71d397a268cac
Ric Richards <richardsric7@gmail.com>
2022-11-06 23:40:06 +0100

    update

--- internal/components/users/services/asset_trusts_and_claims.go
+++ internal/components/users/services/asset_trusts_and_claims.go
@@ -254,7 +254,7 @@ func generateClaimPendingAssetXdr(wallet *userModels.UserWallet, pendingAssetToC
 
 	var asset txnbuild.Asset = nil
 
-	asset = txnbuild.NativeAsset{}
+	// asset = txnbuild.NativeAsset{}
 
 	if len(pendingAssetToClaim.AssetIssuer) > 0 {
 		asset = txnbuild.CreditAsset{Code: pendingAssetToClaim.AssetCode, Issuer: pendingAssetToClaim.AssetIssuer}
@@ -334,7 +334,7 @@ func generateClaimPendingAssetXdr(wallet *userModels.UserWallet, pendingAssetToC
 	if serviceFee.IsPositive() {
 		if pendingAssetToClaim.Multiparty == 1 {
 			//process service fee
-			if len(os.Getenv("SHARED_ACCESS_FEE_ADDRESS")) != 56 {
+			if len(os.Getenv("SHARED_ACCESS_FEE_ASSET_ISSUER")) != 56 {
 				ops = append(ops, &txnbuild.Payment{
 					Destination:   os.Getenv("SHARED_ACCESS_FEE_ADDRESS"),
 					Amount:        os.Getenv("SHARED_ACCESS_FEE_AMOUNT"),
@@ -348,7 +348,7 @@ func generateClaimPendingAssetXdr(wallet *userModels.UserWallet, pendingAssetToC
 					Destination:   os.Getenv("SHARED_ACCESS_FEE_ADDRESS"),
 					Amount:        os.Getenv("SHARED_ACCESS_FEE_AMOUNT"),
 					SourceAccount: wallet.ID,
-					Asset:         txnbuild.CreditAsset{Code: os.Getenv("SHARED_ACCESS_FEE_ASSET_CODE"), Issuer: os.Getenv("trovo-wallet-api")},
+					Asset:         txnbuild.CreditAsset{Code: os.Getenv("SHARED_ACCESS_FEE_ASSET_CODE"), Issuer: os.Getenv("SHARED_ACCESS_FEE_ASSET_ISSUER")},
 				})
 				pendingAssetToClaim.Messages = append(pendingAssetToClaim.Messages, fmt.Sprintf("%v %v will be deducted from wallet %v as service fee.", os.Getenv("SHARED_ACCESS_FEE_AMOUNT"), os.Getenv("SHARED_ACCESS_FEE_ASSET_CODE"), wallet.Alias))
 
