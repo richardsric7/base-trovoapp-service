5f4703444a01ac9b6032ac2f228fa86448262b0f
Kennis Maduka <kent2ckymaduka@gmail.com>
2023-08-28 10:34:48 +0100

    update patron welcome screen to show current patron plan

---
+++ lib/models/patronMembership.dart
@@ -0,0 +1,35 @@
+class PatronMembership {
+  String? username;
+  String? patronPackageId;
+  String? patronTierId;
+  int? price;
+  DateTime? validTill;
+  PatronMembership({
+    this.username,
+    this.patronPackageId,
+    this.patronTierId,
+    this.price,
+    this.validTill,
+  });
+
+  toJSONEncodable() {
+    return <String, dynamic>{
+      "validTill": validTill!.toIso8601String(),
+      "patronPackageId": patronPackageId,
+      "patronTierId": patronTierId,
+      "price": price,
+      "username": username,
+    };
+  }
+
+  PatronMembership deserializeJson(Map<String, dynamic> m) {
+    print('===> deserializing patronMembership ${m}');
+    return PatronMembership(
+      validTill: DateTime.parse(m["validTill"]),
+      patronPackageId: m["patronPackageId"],
+      patronTierId: m["patronTierId"],
+      price: m["price"],
+      username: m["username"],
+    );
+  }
+}
