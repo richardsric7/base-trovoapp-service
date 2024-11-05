e4a808e147b3db5442c224dae5a7ef05e69a3c82
“Ric <richardsric7@gmail.com>
2024-11-05 10:13:28 +0100

    user change

--- internal/components/users/db/user_go.go
+++ internal/components/users/db/user_go.go
@@ -48,7 +48,8 @@ func GetUser(userInfo string, db *gorm.DB, gc *sharedconfig.GlobalConfig) (user 
 
 	} else {
 
-		e = db.Preload("UserWallets.Permissions").Preload(clause.Associations).Where("id = ?", userInfo).Or("username = ?", strings.ToLower(userInfo)).Or("mobile = ?", &userInfo).Or("email = ?", userInfo).First(&user).Error
+		// e = db.Preload("UserWallets.Permissions").Preload(clause.Associations).Where("id = ?", userInfo).Or("username = ?", strings.ToLower(userInfo)).Or("mobile = ?", &userInfo).Or("email = ?", userInfo).First(&user).Error
+		e = db.Preload("UserWallets.Permissions").Preload(clause.Associations).Where("id = ? OR username = ? OR mobile = ? OR email = ?", userInfo, strings.ToLower(userInfo), &userInfo, userInfo).First(&user).Error
 	}
 
 	if e != nil {
