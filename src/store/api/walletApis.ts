d96895186cd5666393e9664aec4d8b863f38ccf8
Kennis Maduka <kent2ckymaduka@gmail.com>
2024-11-11 09:58:29 +0100

    complete receive asset

--- src/store/api/walletApis.ts
+++ src/store/api/walletApis.ts
@@ -16,10 +16,41 @@ export const authApi = baseApi.injectEndpoints({
           }
         }, 
       }),
+    }), 
+    receiveAsset: builder.query({
+      query: (payload: Payload) => {
+        return ({
+          url: `/v1/users/payment/generate/${payload.body.alias}?paymentDestination=${payload.body.publicKey}&assetCode=${payload.body.assetCode}&assetIssuer=${payload.body.assetIssuer}&amount=${payload.body.amount}&memo=${payload.body.memo != null ? encodeURIComponent(payload.body.memo.toString()) : ''}`,
+          method: 'GET',        
+          data: {
+            creds: {
+              signer: payload.signer,
+              publicKey: payload.publicKey,
+              secretKey: payload.secretKey,
+            }
+          }, 
+        })
+      },
+    }),
+    swapAsset: builder.mutation({
+      query: (payload: Payload) => ({        
+        url: payload.body.isSharedWallet ? '/v1/users/swap' : '/v1/users/swap',
+        method: 'POST',        
+        data: {
+          payload: payload.body,
+          creds: {
+            signer: payload.signer,
+            publicKey: payload.publicKey,
+            secretKey: payload.secretKey,
+          }
+        }, 
+      }),
     }),    
   }),
 });
 
 export const {
   useSendAssetMutation,
+  useSwapAssetMutation,
+  useLazyReceiveAssetQuery,
 } = authApi;
