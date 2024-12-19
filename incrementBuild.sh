66576279cc2a539cd70e7314f4a5423eb4b23b47
Kennis Maduka <kent2ckymaduka@gmail.com>
2024-12-19 11:16:09 +0100

    trigger new commit to test pre-commit hook

--- incrementBuild.sh
+++ incrementBuild.sh
@@ -3,6 +3,8 @@
 set -e  # Exit on errors
 set -x  # Print commands as they are executed
 
+# trigger new commit
+
 path_to_pubspec="pubspec.yaml"
 current_version=$(awk '/^version:/ {print $2}' $path_to_pubspec)
 echo "current version: $current_version"
