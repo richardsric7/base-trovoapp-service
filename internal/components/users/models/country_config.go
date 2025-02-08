3d190be7d0fc7fc3260552dd596ba8a59b2869a8
“Ric <richardsric7@gmail.com>
2025-02-08 15:57:32 +0100

    added country config

--- internal/components/users/models/country_config.go
+++ internal/components/users/models/country_config.go
@@ -2,7 +2,8 @@ package users
 
 // Country holds country struct
 type Country struct {
-	CountryCode   string  `gorm:"size:3;primaryKey" json:"countryCode"`
-	SECFeePercent float64 `gorm:"default:0" json:"SECFeePercent"`
-	RegionName    string  `json:"regionName"`
+	CountryCode               string  `gorm:"size:3;primaryKey" json:"countryCode"`
+	SECTokenizationFeePercent float64 `gorm:"default:0" json:"SECTokenizationFeePercent"`
+	SECTradeFeePercent        float64 `gorm:"default:0" json:"SECTradeFeePercent"`
+	RegionName                string  `json:"regionName"`
 }
