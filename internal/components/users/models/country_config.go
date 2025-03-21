2a234f9768abaf91212162299a12a6a8ba99a70a
“Ric <richardsric7@gmail.com>
2025-03-21 19:30:47 +0100

    fix

--- internal/components/users/models/country_config.go
+++ internal/components/users/models/country_config.go
@@ -10,6 +10,7 @@ type Country struct {
 	SECTradeFee                     float64 `gorm:"default:0" json:"SECTradeFeePercent"`
 	SECTradeFeeType                 int     `gorm:"default:0" json:"SECTradeFeeType"` //0=percent,1 = fixed
 	RegionName                      string  `json:"regionName"`
+	CountryName                     string  `json:"countryName"`
 	MinTokenizationFee              float64 `gorm:"default:0" json:"minTokenizationFee"`
 	TokenizationApplicationFee      float64 `gorm:"default:0" json:"tokenizationApplicationFee"`
 	TokenizationApplicationFeeAsset string  `gorm:"default:'TROV:GAXMBPVA2GNG6A3NV6Q664VZASMROS5ZACKSMTPVCRIKPOJIV43A2CTJ'" json:"tokenizationApplicationFeeAsset"`
