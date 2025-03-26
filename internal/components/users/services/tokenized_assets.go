b72879424fc5efeec1619ff365adbfbaf325b54b
“Ric <richardsric7@gmail.com>
2025-03-26 14:29:32 +0100

    fix

--- internal/components/users/services/tokenized_assets.go
+++ internal/components/users/services/tokenized_assets.go
@@ -1071,9 +1071,9 @@ func ActivateSalesRoutine(gc *sharedconfig.GlobalConfig) {
 	{
 		//start primnary sales
 
-		e := gc.DB.Where("Sales_Start::date = now()::date AND Asset_Tokenization_Status = ?", 4).First(&userModels.TokenizedAsset{}).Error
+		e := gc.DB.Where("Sales_Start::date =< now()::date AND Asset_Tokenization_Status = ?", 4).First(&userModels.TokenizedAsset{}).Error
 		if e == nil {
-			result := gc.DB.Where("Sales_Start::date = now()::date AND Asset_Tokenization_Status = ?", 4).FindInBatches(&assets, batchSize, func(tx *gorm.DB, batch int) error {
+			result := gc.DB.Where("Sales_Start::date =< now()::date AND Asset_Tokenization_Status = ?", 4).FindInBatches(&assets, batchSize, func(tx *gorm.DB, batch int) error {
 				for i, asset := range assets {
 
 					//check if it has been minted.
