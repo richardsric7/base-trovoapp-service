9a1cb4dc2a09319ea1a841e3c647274aba5237a0
“Ric <richardsric7@gmail.com>
2025-11-23 14:04:21 +0100

    fix

--- internal/components/users/services/tokenized_assets.go
+++ internal/components/users/services/tokenized_assets.go
@@ -1069,7 +1069,7 @@ func AssignIssuingWallet(tokenizationID string, gc *sharedconfig.GlobalConfig) (
 			primarySignature, subwalletSignature, _, e := middleware.SignSubwalletBase64Txn(tokenizationIssuerProfileWalletKP.Seed(), issuer.Seed(), "", p.Transaction, p.NetworkPassPhrase)
 			if e != nil {
 				log.Printf("[AssignIssuingWallet.SignSubwalletBase64Txn] Error signing issuing wallet with primary, sub and linked wallets [%v] [%v] [%v], err: %v\n", tokenizationIssuerProfileWalletKP.Address(), issuer.Address(), "", e)
-				err = &tErrors.CustomError{Param: "issuingPublicKey", Err: "error-invalid-issuer", ErrMessage: e.Error()}
+				err = &tErrors.CustomError{Param: "issuingPublicKey", Err: "error-invalid-issuer", ErrMessage: "Invalid primary, subwallet or linked wallet Signer"}
 				return
 
 			}
